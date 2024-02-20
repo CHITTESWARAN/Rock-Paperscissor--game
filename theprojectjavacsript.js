608c059f57549fe974a029b038a7da491dc37453
CHITTESWARAN <chitteswarancj06@gmail.com>
2024-02-20 23:56:45 +0530

    code had a writed in the html

---
+++ theprojectjavacsript.js
@@ -0,0 +1,7 @@
+const userScore=0;
+const computerScore=0;
+const userScore_span =document.getElementById("user-score");
+const computerScore_span=document.getElementById("computer-label");
+const scoreBoard_div=document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+
